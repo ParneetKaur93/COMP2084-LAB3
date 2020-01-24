5fc5f6430c86e29c7c4c6e186e6696fc77e171e6
ParneetKaur <200428731@student.georgianc.on.ca>
2020-01-23 22:36:55 -0500

    lab3 solution files

---
+++ Controllers/PhoneController.cs
@@ -0,0 +1,26 @@
+﻿using PhoneApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PhoneApplication.Controllers
+{
+    public class PhoneController : Controller
+    {
+        // GET: Phone
+        public ActionResult Index()
+        {
+
+            var phonelist = new List<Phone>
+                {
+                    new Phone{ PhoneId = 01, PhoneName = "SamsungJ8", Manufacturer = "Samsung"},
+                new Phone { PhoneId = 02, PhoneName ="Oppo", Manufacturer = "Oppo"},
+
+
+            };
+            return View(phonelist);
+        }
+    }
+}
